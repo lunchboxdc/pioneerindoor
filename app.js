91225272ef5340a3d33f2aebcf5d5eedd2eb04e0
Andy <lunchboxdc@gmail.com>
2015-07-26 17:29:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
 app.use(express.static('assets'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(app.get('port'), '127.0.0.1', function() {
+app.listen(app.get('port'), process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', function() {
   console.log('Express server listening on port ' + app.get('port'));
 });

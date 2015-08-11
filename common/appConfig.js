e2b973b165877c6e6c1c366ff588c9d4022f35f1
Andy <lunchboxdc@gmail.com>
2015-08-11 00:40:30 -0500

    log messages in config loader

--- common/appConfig.js
+++ common/appConfig.js
@@ -2,6 +2,7 @@ var logger = require('./logger');
 
 var configObject = {};
 if(process.env.NODE_ENV === 'prod') {
+	console.info('App running in prod mode');
     configObject["host"] = "https://www.lunchboxdc.me";
     configObject["logLevel"] = "error";
 } else {
@@ -9,6 +10,8 @@ if(process.env.NODE_ENV === 'prod') {
     configObject["host"] = "http://localhost:"+configObject.port;
     configObject["logLevel"] = "debug";
 }
+
+console.info('App properties: %s', configObject);
 
 if(configObject.logLevel) {
     logger.transports.console.level = configObject.logLevel;

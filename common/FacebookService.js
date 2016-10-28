6723ab4b02a0c154ebf0baa2f8e0798f39ec97cb
Andy Hull <lunchboxdc@gmail.com>
2016-10-27 22:38:52 -0500

    adding code to delete posts from pi db if the post was previously added then deleted on facebook

--- common/FacebookService.js
+++ common/FacebookService.js
@@ -21,67 +21,93 @@ module.exports = {
                     if (error) {
                         console.error(error.stack);
                     } else {
-                        var json = JSON.parse(body);
-                        async.each(json.data, function(post) {
-                            var story = post.story ? post.story.toLowerCase() : '';
-                            if (story.indexOf("profile picture") < 0 && story.indexOf("cover photo") < 0 && story.indexOf("shared") < 0) {
-                                var facebookPost = {
-                                    fromName: post.from.name,
-                                    name: post.name,
-                                    story: post.story,
-                                    picture: post.picture,
-                                    link: post.link,
-                                    description: post.description,
-                                    caption: post.caption,
-                                    type: post.type,
-                                    status_type: post.status_type,
-                                    created_time: post.created_time
-                                };
+                        FacebookPost.find({}).exec(function(err, facebookPosts) {
+                            if (err) {
+                                res.send(err);
+                            } else {
+                                var posts = JSON.parse(body).data;
 
-                                if (post.message) {
-                                    var maxChars = 140;
-                                    var message;
-                                    if (/\s/.test(post.message.charAt(maxChars))) {
-                                        var lastCharIndex = maxChars;
-                                        for (var i = maxChars; i > -1; i--) {
-                                            if (!/\s/.test(post.message.charAt(i-1))) {
-                                                lastCharIndex = i;
-                                                break;
-                                            }
+                                for (var i = 0; i < facebookPosts.length; i++) {
+                                    var keepPost = false;
+                                    for (var t = 0; t < posts.length; t++) {
+                                        if (posts[t].id === facebookPosts[i]._id) {
+                                            keepPost = true;
+                                            break;
                                         }
-                                        message = post.message.substr(0, lastCharIndex);
-                                    } else {
-                                        var nextSpaceIndex;
-                                        for (var i = maxChars; i < post.message.length; i++) {
-                                            var char = post.message.charAt(i);
-                                            if (/\s/.test(char)) {
-                                                nextSpaceIndex = i;
-                                                break;
+                                    }
+                                    if (!keepPost) {
+                                        FacebookPost.remove({_id: facebookPosts[i]._id}, function(err) {
+                                            if (err) {
+                                                res.send(err);
+                                            } else {
+                                                res.json({message: 'Deleted old facebook post'});
                                             }
-                                        }
-
-                                        message = post.message.substr(0, nextSpaceIndex);
+                                        });
                                     }
-
-                                    if (!post.object_id) {
-                                        post.object_id = post.id.substring(post.id.indexOf('_') + 1, post.id.length);
-                                    }
-
-                                    facebookPost.message = message + '... <a href="https://www.facebook.com/PioneerIndoor/posts/' + post.object_id + '" target="_blank">See More</a>';
                                 }
 
-                                try {
-                                    facebookPost.attachmentImage = post.attachments.data[0].media.image.src;
-                                } catch (e) {
-                                }
+                                async.each(posts, function(post) {
+                                    var story = post.story ? post.story.toLowerCase() : '';
+                                    if (story.indexOf("profile picture") < 0 && story.indexOf("cover photo") < 0 && story.indexOf("shared") < 0) {
+                                        var facebookPost = {
+                                            fromName: post.from.name,
+                                            name: post.name,
+                                            story: post.story,
+                                            picture: post.picture,
+                                            link: post.link,
+                                            description: post.description,
+                                            caption: post.caption,
+                                            type: post.type,
+                                            status_type: post.status_type,
+                                            created_time: post.created_time
+                                        };
 
-                                FacebookPost.findOneAndUpdate({
-                                    _id: post.id
-                                }, facebookPost, {upsert: true}, function(err, post) {
-                                    if (err) {
-                                        console.error(err);
-                                    } else {
-                                        console.debug('FacebookService: upserted post.');
+                                        if (post.message) {
+                                            var maxChars = 140;
+                                            var message;
+                                            if (/\s/.test(post.message.charAt(maxChars))) {
+                                                var lastCharIndex = maxChars;
+                                                for (var i = maxChars; i > -1; i--) {
+                                                    if (!/\s/.test(post.message.charAt(i-1))) {
+                                                        lastCharIndex = i;
+                                                        break;
+                                                    }
+                                                }
+                                                message = post.message.substr(0, lastCharIndex);
+                                            } else {
+                                                var nextSpaceIndex;
+                                                for (var i = maxChars; i < post.message.length; i++) {
+                                                    var char = post.message.charAt(i);
+                                                    if (/\s/.test(char)) {
+                                                        nextSpaceIndex = i;
+                                                        break;
+                                                    }
+                                                }
+
+                                                message = post.message.substr(0, nextSpaceIndex);
+                                            }
+
+                                            if (!post.object_id) {
+                                                post.object_id = post.id.substring(post.id.indexOf('_') + 1, post.id.length);
+                                            }
+
+                                            facebookPost.message = message + '... <a href="https://www.facebook.com/PioneerIndoor/posts/' + post.object_id + '" target="_blank">See More</a>';
+                                        }
+
+                                        try {
+                                            facebookPost.attachmentImage = post.attachments.data[0].media.image.src;
+                                        } catch (e) {
+                                        }
+
+                                        FacebookPost.findOneAndUpdate({
+                                            _id: post.id
+                                        }, facebookPost, {upsert: true}, function(err, post) {
+                                            if (err) {
+                                                console.error(err);
+                                            } else {
+                                                console.debug('FacebookService: upserted post.');
+                                            }
+                                        });
                                     }
                                 });
                             }
